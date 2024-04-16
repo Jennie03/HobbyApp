229f7b59d7a87c757f8f832d20280ba8bf9ff93d
Jennie03 <s160421039@student.ubaya.ac.id>
2024-04-16 17:30:30 +0700

    layout login, regist, fragment news

--- app/src/main/java/com/ubaya/hobbyapp/view/MainActivity.kt
+++ app/src/main/java/com/ubaya/hobbyapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ubaya.hobbyapp
+package com.ubaya.hobbyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ubaya.hobbyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
